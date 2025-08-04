a912634a0b2f9729b3aa93731fe92555c4d6544a
Julián Manzano <jmanzanm@emeal.nttdata.com>
2025-08-04 12:51:18 +0200

    Añadido . a ultimo echo

--- saludo.sh
+++ saludo.sh
@@ -1,3 +1,3 @@
 echo "Hola mundo con Jenkins y GitHub."
 echo "Probando trigger desde github y jenkins."
-echo "Probando trigger con webhook desde github usando grok"
+echo "Probando trigger con webhook desde github usando grok."
